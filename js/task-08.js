4b4c684f1d3ce54ed26be5113d62ecc50caa8936
Dawid Czyzewski <vinciebr@proton.me>
2023-02-25 19:47:36 +0100

    Finished all exercises

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,60 @@
+"use strict:";
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", (submit) => {
+  submit.preventDefault();
+
+  const login = submit.currentTarget.elements[0].value;
+  const password = submit.currentTarget.elements[1].value;
+
+  if (login === "" || password === "") {
+    alert("Please fill all the labels");
+  } else {
+    const object = { login, password };
+    console.log(object);
+  }
+  submit.currentTarget.reset();
+});
+
+// ---------------------- Clear code upper ------------------------------------------------
+// ---------------------- Just comment upper and uncomment  these lines -------------------
+// ---------------------- Code with comments and logs under -------------------------------
+
+// // Geting elements, which will be nessesery to listen and change
+// const form = document.querySelector(".login-form");
+
+// // Checking elements of form
+// // console.log("form: ", form);
+// // console.log("form[0].elements: ", form[0].elements);
+// // console.log("form[0].elements: ", form[0].elements);
+
+// form.addEventListener("submit", (submit) => {
+//   submit.preventDefault();
+//   //   console.log("checking submit: ", submit);
+//   //   console.log("elements of submit.target: ", submit.target);
+//   //   console.log("submit.target[0]: ", submit.target[0]); // email is here
+//   //   console.log("submit.target[1]: ", submit.target[1]); // password is here
+//   //   const login = submit.target[0].value;
+//   //   const password = submit.target[1].value;
+
+//   //   // But our task is to use "elements"
+//   //   console.log(
+//   //     "We have it alse here, as we have in instruction to task: ",
+//   //     submit.currentTarget.elements
+//   //   );
+
+//   const login = submit.currentTarget.elements[0].value;
+//   const password = submit.currentTarget.elements[1].value;
+
+//   // console.log("login value: ", login);
+//   // console.log("password value: ", password);
+
+//   if (login === "" || password === "") {
+//     alert("Please fill all the labels");
+//   } else {
+//     const object = { login, password };
+//     console.log(object);
+//   }
+//   submit.currentTarget.reset();
+// });
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,46 @@
+"use strict:";
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const span = document.querySelector(".color");
+
+const changeColor = () => {
+  let color = getRandomHexColor();
+  body.style.background = color;
+  span.textContent = color;
+};
+
+button.addEventListener("click", changeColor);
+
+// ---------------------- Clear code upper ------------------------------------------------
+// ---------------------- Just comment upper and uncomment  these lines -------------------
+// ---------------------- Code with comments and logs under -------------------------------
+
+// // default function:
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// // Geting elements, which will be nessesery to listen and change
+// const button = document.querySelector(".change-color");
+// const body = document.querySelector("body");
+// const span = document.querySelector(".color");
+
+// // console.log("button element: ", button);
+
+// // creating function to add effect of getRandomHexColor as background color and name of it to span
+// const changeColor = () => {
+//   // function changeColor() {
+//   // console.log("button clicked");
+//   // console.log("result of getRandomHexColor function: ", getRandomHexColor());
+//   let color = getRandomHexColor()
+//   body.style.background = color;
+//   span.textContent = color;
+// };
+
+// // Adding event listener with function changeColor to button:
+// button.addEventListener("click", changeColor);
